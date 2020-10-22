73018e3ca952073ee064f347faf02349c01bf11c
AndreSanches <sanchessls@gmail.com>
2020-10-22 10:58:14 +0100

    v 3.2

--- LearnEnglishWords/LearnEnglishWords/Form2.Designer.cs
+++ LearnEnglishWords/LearnEnglishWords/Form2.Designer.cs
@@ -42,17 +42,19 @@
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(35, 66);
+            this.checkedListBox1.Location = new System.Drawing.Point(47, 81);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(355, 364);
+            this.checkedListBox1.Size = new System.Drawing.Size(472, 446);
             this.checkedListBox1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button1.Location = new System.Drawing.Point(130, 445);
+            this.button1.Location = new System.Drawing.Point(173, 548);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.Size = new System.Drawing.Size(165, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Confirmar";
             this.button1.UseVisualStyleBackColor = false;
@@ -66,34 +68,38 @@
             "I\'m With you",
             "Naked",
             "Anything But Ordinary"});
-            this.comboBox1.Location = new System.Drawing.Point(447, 66);
+            this.comboBox1.Location = new System.Drawing.Point(596, 81);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(345, 21);
+            this.comboBox1.Size = new System.Drawing.Size(459, 24);
             this.comboBox1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(127, 50);
+            this.label1.Location = new System.Drawing.Point(169, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.Size = new System.Drawing.Size(128, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Treino de Palavras";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(444, 39);
+            this.label2.Location = new System.Drawing.Point(592, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(52, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Provas";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(447, 93);
+            this.button2.Location = new System.Drawing.Point(596, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.Size = new System.Drawing.Size(165, 28);
             this.button2.TabIndex = 5;
             this.button2.Text = "Confirmar";
             this.button2.UseVisualStyleBackColor = true;
@@ -102,18 +108,20 @@
             // HardMode
             // 
             this.HardMode.AutoSize = true;
-            this.HardMode.Location = new System.Drawing.Point(282, 437);
+            this.HardMode.Location = new System.Drawing.Point(376, 538);
+            this.HardMode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HardMode.Name = "HardMode";
-            this.HardMode.Size = new System.Drawing.Size(96, 17);
+            this.HardMode.Size = new System.Drawing.Size(123, 21);
             this.HardMode.TabIndex = 6;
             this.HardMode.Text = "Hard Mode On";
             this.HardMode.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(447, 186);
+            this.button3.Location = new System.Drawing.Point(596, 229);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.Size = new System.Drawing.Size(165, 28);
             this.button3.TabIndex = 7;
             this.button3.Text = "Confirmar";
             this.button3.UseVisualStyleBackColor = true;
@@ -122,17 +130,18 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(444, 161);
+            this.label3.Location = new System.Drawing.Point(592, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.Size = new System.Drawing.Size(91, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Minimal Pairs";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(823, 518);
+            this.ClientSize = new System.Drawing.Size(1097, 638);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.HardMode);
@@ -142,7 +151,10 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(533, 492);
             this.Name = "Form2";
+            this.Padding = new System.Windows.Forms.Padding(11, 10, 11, 10);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
